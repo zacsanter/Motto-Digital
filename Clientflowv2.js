95d0ac4a55cdfbf475fb6bae01b3a7bb22c2eb94
zacsanter <53116184+zacsanter@users.noreply.github.com>
2024-06-18 14:25:36 +0100

    Update and rename Clientflow.js to Clientflowv2.js

--- Clientflowv2.js
+++ Clientflowv2.js
@@ -1,4 +1,4 @@
-const typingIndicator = document.getElementById("typing-indicator");
+onst typingIndicator = document.getElementById("typing-indicator");
 const uniqueId = generateUniqueId();
 const voiceflowRuntime = "general-runtime.voiceflow.com";
 const voiceflowVersionID =
